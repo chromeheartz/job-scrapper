f6105f90c41b8d99a7e6b5a645e9eb17eab348fd
chromeheartz <barnesquiat@gmail.com>
2022-09-15 01:15:35 +0900

    #5.4 beautiful soup

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+"""
+    사용한 라이브러리
+    requsets
+    beautifulSoup
+"""
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "react"
@@ -7,4 +13,7 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
   print("cant't request website")
 else:
-  print(response.text)
+  # beautifulsoup한테 html을 보내준다고말함
+  soup = BeautifulSoup(response.text, "html.parser")
+  # class=""이 아닌 class_="" 가 중요하다
+  print(soup.find_all('section', class_="jobs")) 
