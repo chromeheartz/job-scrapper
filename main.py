3c61e79cdb8d064ac1b55bda013ed340746ac47c
chromeheartz <barnesquiat@gmail.com>
2022-09-17 21:15:40 +0900

    #5.7 job extraction

--- main.py
+++ main.py
@@ -24,10 +24,21 @@ else:
       #pop은 list에서 한 항목을 제거. -1로 마지막 제거
       job_posts.pop(-1)
       for post in job_posts:
-          print(post)
-          print("/////////////////")
+          anchors = post.find_all('a')
+            anchor = anchors[1]
+            # beautiful soup은 dictionary처럼 반환해주기 때문에 이렇게 찾는것도가능
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_="company")
+            # print(company)
+            title = anchor.find('span', class_="title")
+            print(company,"\n", title)
+            print("///////////////////////////////////////")
+          
 
 """
+
+  category of arguments
+
   class_="jobs"는 keyword argument라고 한다
   우리한테 익숙한것은 positional argument이다.
 
@@ -38,5 +49,15 @@ else:
 """
 
 """
-  setting
+
+  dictionary's short cut
+
+  python에서 object list를 가지고있고 list의 길이를 안다면
+  list에서 object를 가져오는 코드가있다
+
+  list_of_numbers = [1,2,3]
+  # 이런씩으로 하나하나 가져오는것이 아닌
+  first = list_of_numbers[1]
+  # 각 위치에맞게 변수지정가능. 하지만 길이를 알아야함
+  first, second, third = list_of_numbers
 """
