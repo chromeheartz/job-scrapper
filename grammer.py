423c5905ee3c5cc634bc95aa240886c7e2bf0f15
chromeheartz <barnesquiat@gmail.com>
2022-09-19 22:30:23 +0900

    #5.13 None

---
+++ grammer.py
@@ -0,0 +1,38 @@
+
+
+"""
+
+  # category of arguments
+
+  class_="jobs"는 keyword argument라고 한다
+  우리한테 익숙한것은 positional argument이다.
+
+  positional argument은 위치에 따라 무엇이 올지 아는것
+  keyword argument는 자리에 더이상 신경쓰지않을때 중요해진다
+  자리에 신경안쓰는대신 argument이름에 신경을쓰는것
+  변수명="넘겨줄인자" 이런식으로 써줌. 순서는 신경안써도괜찮음
+"""
+
+"""
+
+  # dictionary's short cut
+
+  python에서 object list를 가지고있고 list의 길이를 안다면
+  list에서 object를 가져오는 코드가있다
+
+  list_of_numbers = [1,2,3]
+  # 이런씩으로 하나하나 가져오는것이 아닌
+  first = list_of_numbers[1]
+  # 각 위치에맞게 변수지정가능. 하지만 길이를 알아야함
+  first, second, third = list_of_numbers
+"""
+
+"""
+  # None
+  
+  None은 True,False,숫자,string처럼
+  변수에 넣을수도있고, list나 dictionary안에 넣어도 되는 데이터 타입이다
+
+  None은 무언가가 없을때 쓴다. False와는 다르다 
+  False는 True가 아닌것을 의미하고 None은 무언가가 "없을때"
+"""
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 """
     사용한 라이브러리
     requsets
-    # python코드에서 웹사이트로 request를 보낼수있게 해준다
+    python코드에서 웹사이트로 request를 보낼수있게 해준다
     get 은 function인데 이동한 다음에 website를 가져오는것
 
     beautifulSoup
-    # find_all같은 document에서 HTML태그를 찾게 해준다
+    find_all같은 document에서 HTML태그를 찾게 해준다
     html을 첫번째 argument로 전해주고 html.parser라는 문자열을 전달해줌
     이것은 내가 beautifulSoup한테 html을 보내준다고 말하는것
-"""
 
-# selenium은 브라우저를 자동화 할수있는 프로그램
-# webdriver는 파이썬에서 브라우저를 시작할 수 있는 방법
+    selenium
+    selenium은 브라우저를 자동화 할수있는 프로그램
+    webdriver는 파이썬에서 브라우저를 시작할 수 있는 방법
+"""
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 #options의 instance라는것을 알려줌
 options = Options()
@@ -70,31 +72,4 @@ else:
   for result in results:
   print(result)
   print("//////////////")
-"""
-
-"""
-
-  category of arguments
-
-  class_="jobs"는 keyword argument라고 한다
-  우리한테 익숙한것은 positional argument이다.
-
-  positional argument은 위치에 따라 무엇이 올지 아는것
-  keyword argument는 자리에 더이상 신경쓰지않을때 중요해진다
-  자리에 신경안쓰는대신 argument이름에 신경을쓰는것
-  변수명="넘겨줄인자" 이런식으로 써줌. 순서는 신경안써도괜찮음
-"""
-
-"""
-
-  dictionary's short cut
-
-  python에서 object list를 가지고있고 list의 길이를 안다면
-  list에서 object를 가져오는 코드가있다
-
-  list_of_numbers = [1,2,3]
-  # 이런씩으로 하나하나 가져오는것이 아닌
-  first = list_of_numbers[1]
-  # 각 위치에맞게 변수지정가능. 하지만 길이를 알아야함
-  first, second, third = list_of_numbers
 """
