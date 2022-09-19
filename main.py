6b94c21bfd88b851a9543c49bd9d6bde39d75930
chromeheartz <barnesquiat@gmail.com>
2022-09-19 22:31:05 +0900

    None 2

--- main.py
+++ main.py
@@ -26,8 +26,14 @@ options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=options)
 
 browser.get("https://kr.indeed.com/jobs?q=python&limit=50")
+soup = BeautifulSoup(browser.page_source, 'html.parser')
 
-print(browser.page_source)
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+    zone = job.find('div', class_="mosaic-zone")
+    if zone == None:
+        print("job li")
 
 """
 from requests import get
