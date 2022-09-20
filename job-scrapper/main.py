f8910e126496c871f1c25f83a3512c345443b570
chromeheartz <barnesquiat@gmail.com>
2022-09-20 23:12:40 +0900

    #5.17 Refactor

--- job-scrapper/main.py
+++ job-scrapper/main.py
@@ -34,39 +34,43 @@ def get_page_count(keyword):
       return 1
     pagination = soup.find("div", class_="pagination-list")
     pages = pagination.find_all("li", recursive=False)
-    print(len(pages))
+    count = len(pages)
+    if count >= 5:
+      return 5
+    else:
+      return count
 
 
 get_page_count("python")
 
 
 def extract_indeed_jobs(keyword):
+    #range 는 순서객체를 return해주는 함수, 즉 list같은것을 즉석으로 만들수있다(튜플?)
+    pages = get_page_count(keyword)
+    for page in range(pages):
+      soup = BeautifulSoup(browser.page_source, 'html.parser')
+      job_list = soup.find("ul", class_="jobsearch-ResultsList")
+      jobs = job_list.find_all("li", recursive=False)
+      for job in jobs:
+          zone = job.find("div", class_="mosaic-zone")
+          if zone == None:
+              results = []
+              # h2를 찾고 a를 찾는 대신 select로 selector를 이용해찾음
+              # beautifulSoup은 HTML태그들을 데이터구조로 가져온다 list,dictionary
+              anchor = job.select_one(".jobTitle a")
+              title = anchor['aria-label']
+              link = anchor['href']
+              # 정보와 위치 찾기
+              company = job.find("span", class_="companyName")
+              location = job.find("div", class_="companyLocation")
+              # 데이터 추출위해 정리
+              job_data = {
+                  'link': f"https://kr.indeed.com{link}",
+                  'company': company.string,
+                  'location ': location.string,
+                  'position': title
+              }
+              results.append(job_data)
 
-    browser.get(f"https://kr.indeed.com/jobs?q={keyword}&limit=50")
-
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
-    job_list = soup.find("ul", class_="jobsearch-ResultsList")
-    jobs = job_list.find_all("li", recursive=False)
-    for job in jobs:
-        zone = job.find("div", class_="mosaic-zone")
-        if zone == None:
-            results = []
-            # h2를 찾고 a를 찾는 대신 select로 selector를 이용해찾음
-            # beautifulSoup은 HTML태그들을 데이터구조로 가져온다 list,dictionary
-            anchor = job.select_one(".jobTitle a")
-            title = anchor['aria-label']
-            link = anchor['href']
-            # 정보와 위치 찾기
-            company = job.find("span", class_="companyName")
-            location = job.find("div", class_="companyLocation")
-            # 데이터 추출위해 정리
-            job_data = {
-                'link': f"https://kr.indeed.com{link}",
-                'company': company.string,
-                'location ': location.string,
-                'position': title
-            }
-            results.append(job_data)
-
-    for result in results:
-        print(result)
+      for result in results:
+          print(result)
