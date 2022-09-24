162e6e866b89316acfd0123fef4ffe1e9d883677
chromeheartz <barnesquiat@gmail.com>
2022-09-24 17:37:34 +0900

    #6.2 Render Template

--- job-scrapper/main.py
+++ job-scrapper/main.py
@@ -13,10 +13,9 @@
     selenium은 브라우저를 자동화 할수있는 프로그램
     webdriver는 파이썬에서 브라우저를 시작할 수 있는 방법
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("jobScrapper")
-
 """
   @가 있는 코드를 함수의 위에 위치
   decorator라고도 함. syntactic sugar
@@ -24,9 +23,17 @@ app = Flask("jobScrapper")
   user가 이 주소의 page를 방문했을 때
   함수를 호출해야하는것을 알게된다
 """
+
+
 @app.route('/')
 def home():
-    return "hey there!"
+    # python은 백엔드다. bibi라는 데이터,문자열은 백엔드에서부터 전달되고있다.
+    return render_template("home.html", name="bibi")
+
+
+@app.route("/hello")
+def hello():
+    return 'hello you!'
 
 
 app.run("0.0.0.0")
